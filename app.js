e7de857bb695e414b62e0ebfe704615eda457efe
Pokhvalenskii <pokhvalenskii@gmail.com>
2021-04-08 19:10:01 +0300

    feat: add `cardSchema`

--- app.js
+++ app.js
@@ -26,7 +26,34 @@ const userSchema = new mongoose.Schema({
     required: true,
     type: String
   }
+});
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+    type: String
+  },
+  link: {
+    required: true,
+    type: String
+  },
+  owner: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    default: []
+  }],
+  createdAt: {
+    default: Date.now
+  }
 })
+
+mongoose.model('user', userSchema);
+mongoose.model('card', cardSchema);
 
 
 
