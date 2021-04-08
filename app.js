38d55a55103cc9164f7ba664883eb225d45d07f4
Pokhvalenskii <pokhvalenskii@gmail.com>
2021-04-08 18:52:34 +0300

    feat: add `userSchema`

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const { PORT = 3000 } = process.env;
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+const userSchema = new mongoose.Schema({
+  name: {
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+    type: String
+  },
+  about: {
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+    type: String
+  },
+  avatar: {
+    required: true,
+    type: String
+  }
+})
+
+
+
+
 app.get('/', (req, res) => {
   res.send('hello server');
-})
+});
+
+
 
 app.listen(PORT, () => {
   console.log('server started')
