ab49ea4c820d7c031a7b6d4d26800ee4f7f224fa
Pokhvalenskii <pokhvalenskii@gmail.com>
2021-04-09 20:36:24 +0300

    ref: update user/avatar

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,7 @@ const postUsers = (req, res) => {
   User.create(date)
     .then((user) => res.send(user))
     .catch((err) => {
+      console.log('POST ERR', err.name);
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: `${Object.values(err.errors).map((error) => error.message).join(', ')}` });
       } else {
@@ -25,17 +26,25 @@ const getUserById = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
     .then((user) => {
+      console.log(user);
       if (!user) {
         res.status(404).send({ message: '404 — Пользователь по указанному _id не найден.' });
       } res.send(user);
     })
-    .catch(() => { res.status(500).send({ message: 'Ошибка сервера' }); });
+    .catch((err) => {
+      console.log('erRRRRRr', err.name);
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: '400 - Переданы некорректные данные при запросе' });
+      } else {
+        res.status(500).send({ message: 'Ошибка сервера' });
+      }
+    });
 };
 
 const updateUser = (req, res) => {
   // console.log('UPDATE USER')
   const { name, about } = req.body;
-  User.findOneAndUpdate(req.user._id, { name, about })
+  User.findOneAndUpdate(req.user._id, { name:name, about:about })
     .then((user) => {
       res.send(user);
     })
@@ -45,14 +54,21 @@ const updateUser = (req, res) => {
 const updateAvatar = (req, res) => {
   // console.log('UPDATE AVATAR')
   const { avatar } = req.body;
-  User.findOneAndUpdate(req.user._id, { avatar })
+  User.findOneAndUpdate(req.user._id, { avatar: avatar })
     .then((user) => {
+      console.log('AVATAR', req.user._id);
       if (!user) {
         res.status(400).send({ message: '400 — Переданы некорректные данные при обновлении профиля.' });
-      }
-      res.send(user);
+      } res.send(user);
     })
-    .catch(() => { res.status(404).send({ message: '404 — Пользователь с указанным _id не найден.' }); });
+    .catch((err) => {
+      console.log('erRRRRRr', err.name);
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: '400 - Переданы некорректные данные при запросе' });
+      } else {
+        res.status(500).send({ message: 'Ошибка сервера' });
+      }
+    });
 };
 
 module.exports = {
